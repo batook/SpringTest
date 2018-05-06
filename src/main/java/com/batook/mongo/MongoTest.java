9cdeda6f8e02bfb3dbd1716296a3f3056284fc8b
batook <batook@mail.ru>
2018-05-06 19:08:51 +0300

    test

--- src/main/java/com/batook/mongo/MongoTest.java
+++ src/main/java/com/batook/mongo/MongoTest.java
@@ -1,42 +1,58 @@
 package com.batook.mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.WriteResult;
 import com.mongodb.client.MongoCursor;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @ContextConfiguration(classes = MongoConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MongoTest {
 
-    @Autowired
     MongoOperations mongoOps;
 
     @Autowired
     MongoClient mongo;
 
+    @Before
+    public void drop() {
+        mongoOps = new MongoTemplate(mongo, "database");
+        mongoOps.dropCollection("person");
+    }
+
     @Test
     public void test() {
         assertNotNull(mongo);
+        assertNotNull(mongoOps);
         MongoCursor<String> i = mongo.listDatabaseNames().iterator();
         while (i.hasNext()) {
             System.out.println(i.next());
         }
-        mongoOps = new MongoTemplate(mongo, "database");
         mongoOps.insert(new Person("Joe", 34));
         assertEquals("Joe", mongoOps.findOne(new Query(where("name").is("Joe")), Person.class).getName());
-        assertEquals(1,mongoOps.getCollection("person").count());
-        mongoOps.dropCollection("person");
+        assertEquals(1, mongoOps.getCollection("person").count());
+        Query query = new Query(where("name").is("Sergey").and("age").is(45));
+        Update update = new Update().set("age", 18);
+        WriteResult wr = mongoOps.upsert(query, update, Person.class);
+        System.out.println(wr);
+        Person p = mongoOps.findOne(new Query(where("name").is("Sergey")), Person.class);
+        assertThat(p.getAge(), is(18));
+        System.out.println(mongoOps.getCollection("person").count());
+        mongoOps.remove(new Query(where("name").is("Sergey")), Person.class);
+        assertEquals(1, mongoOps.getCollection("person").count());
     }
 }
 
