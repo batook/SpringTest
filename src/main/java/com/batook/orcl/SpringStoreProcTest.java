25e51828c01aae7a1f423d0ed8ef60039b309bb4
batook <batook@mail.ru>
2018-05-21 14:37:54 +0300

    Init

--- src/main/java/com/batook/orcl/SpringStoreProcTest.java
+++ src/main/java/com/batook/orcl/SpringStoreProcTest.java
@@ -13,19 +13,6 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-//CREATE OR REPLACE TYPE TEST_EMP_OBJ AS OBJECT (empno number, empname varchar2(30));
-//create or replace type TEST_EMP_OBJ_ARRAY as table of TEST_EMP_OBJ;
-//create table test_emp (empno number, empname varchar2(30));
-//
-//CREATE OR REPLACE PROCEDURE TEST_EMP_OBJ_ARRAY_PROC ( p_obj_array in TEST_EMP_OBJ_ARRAY ) AS
-//begin
-//for i in 1..p_obj_array.count loop
-//insert into test_emp (empno, empname)
-//values(p_obj_array(i).empno, p_obj_array(i).empname);
-//end loop;
-//end;
-
-
 public class SpringStoreProcTest {
     public static void main(String[] args) {
         EmployeeObj emp = new EmployeeObj("TEST_EMP_OBJ", 234, "hello");
