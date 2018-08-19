15cde7ffff918d3fcd59e74287d3331780540546
batook <batook@mail.ru>
2018-08-19 12:08:35 +0300

    Cucumber

--- src/test/features/MyTest.feature
+++ src/test/features/MyTest.feature
@@ -5,3 +5,4 @@ Feature: MyTest
 
   Scenario: Test1
     Given Execute test with param "crap"
+    And Execute test with param "happens"
--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/features", glue = "steps", tags = "@all", format = "pretty")
+@CucumberOptions(features = "src/test/features", glue = "steps", tags = "@all", plugin = {"pretty", "html:target/cucumber"}, monochrome = true)
 public class CucumberTest {
 }
