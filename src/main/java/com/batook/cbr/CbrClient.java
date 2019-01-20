ab89f1a639896ad8f4c9cc3946617c51cb318408
batook <batook@mail.ru>
2019-01-20 14:56:15 +0300

    mbp

--- src/main/java/com/batook/cbr/CbrClient.java
+++ src/main/java/com/batook/cbr/CbrClient.java
@@ -8,7 +8,6 @@ import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.GregorianCalendar;
